e0564e926fcc029d296b26652f11b9c626543ff3
MikhaylovMaks <maksskamm19@bk.ru>
2025-09-20 23:19:29 +0300

    Edit 6

--- 10/main.go
+++ 10/main.go
@@ -12,10 +12,8 @@ func main() {
 	for _, t := range temps {
 		var key int
 		if t < 0 {
-			// для отрицательных округляем "вверх" к нулю
 			key = int(math.Ceil(t/10) * 10)
 		} else {
-			// для положительных вниз
 			key = int(math.Floor(t/10) * 10)
 		}
 		groups[key] = append(groups[key], t)
